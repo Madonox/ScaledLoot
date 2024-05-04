015bf88799cd49260582f88d83b78e976d38015e
Madonox <39967871+Madonox@users.noreply.github.com>
2024-05-04 18:37:33 -0400

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,23 +1,24 @@
 using BepInEx;
 using BepInEx.Configuration;
-using BepInEx.Logging;
 
 namespace ScaledLoot
 {
-    [BepInPlugin("Madonox.ScaledLoot","Scaled Loot","0.0.1")]
+    [BepInPlugin("Madonox.ScaledLoot", "ScaledLoot", "0.0.1")]
     public class Plugin : BaseUnityPlugin
     {
-        internal static ManualLogSource? logger;
-        internal static int QuotaScale = 2500; // quota scale, can be configured
-        public static Config? PluginConfig;
+        private static int QuotaScale = 2500; // quota scale, can be configured
+        //private ConfigEntry<int> configQuota;
+        //public static Config? PluginConfig { get; internal set; }
 
         private void Awake()
         {
-            logger = Logger;
+            Logger.LogInfo("Initializing ScaledLoot.");
             On.DepositItemsDesk.SellItemsOnServer += DepositItemsDesk_SellItemsOnServer;
-            PluginConfig = new(Config);
-            QuotaScale = PluginConfig.getConfigQuotaScale();
-            logger.LogInfo("ScaledLoot started.");
+
+            //configQuota = Config.Bind("General", "QuotaScale",2500,"The quota scale value.");
+            QuotaScale = Config.Bind("General", "QuotaScale", 2500, "The quota scale value.").Value;
+
+            Logger.LogInfo("ScaledLoot has started.  Using a QuotaScale of " + QuotaScale.ToString());
         }
 
         private void DepositItemsDesk_SellItemsOnServer(On.DepositItemsDesk.orig_SellItemsOnServer orig, DepositItemsDesk self)
@@ -39,27 +40,4 @@ namespace ScaledLoot
             orig(self);
         }
     };
-
-    public class Config
-    {
-        public static ConfigEntry<int>? configQuotaScale;
-        public Config(ConfigFile cfg)
-        {
-            configQuotaScale = cfg.Bind(
-                "General",
-                "QuotaScale",
-                2500,
-                "The amount all scrap value is multiplied by, following the equation newScrapValue = initialScrapValue * (currentQuota / QuotaScale)"
-                );
-        }
-
-        public int getConfigQuotaScale()
-        {
-            if (configQuotaScale != null)
-            {
-                return configQuotaScale.Value;
-            }
-            return 2500; // defaults
-        }
-    }
 }
