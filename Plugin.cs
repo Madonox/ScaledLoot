180bffa2e2af2d5ca68dbb30973546b809c016e3
Madonox <39967871+Madonox@users.noreply.github.com>
2024-05-04 21:04:59 -0400

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,37 +1,55 @@
+using System.Collections;
 using BepInEx;
 using BepInEx.Configuration;
+using BepInEx.Logging;
 
 namespace ScaledLoot
 {
-    [BepInPlugin("Madonox.ScaledLoot", "ScaledLoot", "0.0.1")]
+    [BepInPlugin("Madonox.ScaledLoot", "ScaledLoot", "0.0.2")]
     public class Plugin : BaseUnityPlugin
     {
         private static int QuotaScale = 2500; // quota scale, can be configured
         //private ConfigEntry<int> configQuota;
         //public static Config? PluginConfig { get; internal set; }
+        private static ManualLogSource? logger;
 
         private void Awake()
         {
             Logger.LogInfo("Initializing ScaledLoot.");
-            On.DepositItemsDesk.SellItemsOnServer += DepositItemsDesk_SellItemsOnServer;
+            logger = Logger;
+            On.DepositItemsDesk.SellItemsOnServer += SellItemsOnServer;
 
             //configQuota = Config.Bind("General", "QuotaScale",2500,"The quota scale value.");
             QuotaScale = Config.Bind("General", "QuotaScale", 2500, "The quota scale value.").Value;
+            if (QuotaScale < 1)
+            {
+                QuotaScale = 1;
+            }
 
             Logger.LogInfo("ScaledLoot has started.  Using a QuotaScale of " + QuotaScale.ToString());
         }
 
-        private void DepositItemsDesk_SellItemsOnServer(On.DepositItemsDesk.orig_SellItemsOnServer orig, DepositItemsDesk self)
+        private static void SellItemsOnServer(On.DepositItemsDesk.orig_SellItemsOnServer orig, DepositItemsDesk self)
         {
-            int profQuota = TimeOfDay.Instance.profitQuota;
-            if (profQuota > QuotaScale)
+            if (logger != null)
             {
-                // correct scrap value multiplier
-                int delta = profQuota / QuotaScale;
-                for (int i = 0; i < self.itemsOnCounter.Count; i++)
+                logger.LogInfo("Getting TimeOfDay");
+                TimeOfDay instance = TimeOfDay.Instance;
+                if (self.IsServer && instance != null)
                 {
-                    GrabbableObject item = self.itemsOnCounter[i];
-                    item.SetScrapValue((int)item.scrapValue * delta);
+                    logger.LogInfo("Server check, checking profit quota");
+                    int profQuota = TimeOfDay.Instance.profitQuota;
+                    if (profQuota > QuotaScale) // make sure we don't divide because of <1 multiplicatives
+                    {
+                        logger.LogInfo("Using delta " + profQuota);
+                        // correct scrap value multiplier
+                        int delta = profQuota / QuotaScale;
+                        foreach (GrabbableObject item in self.itemsOnCounter)
+                        {
+                            item.SetScrapValue(item.scrapValue * delta);
+                        }
+                        logger.LogInfo("Scrap values updated");
+                    }
                 }
             }
 
