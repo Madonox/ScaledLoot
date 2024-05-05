766e8a71725a2df54846e4faac6edd7b5a946683
Madonox <39967871+Madonox@users.noreply.github.com>
2024-05-04 21:26:09 -0400

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,4 @@
-using System.Collections;
 using BepInEx;
-using BepInEx.Configuration;
-using BepInEx.Logging;
 
 namespace ScaledLoot
 {
@@ -11,12 +8,10 @@ namespace ScaledLoot
         private static int QuotaScale = 2500; // quota scale, can be configured
         //private ConfigEntry<int> configQuota;
         //public static Config? PluginConfig { get; internal set; }
-        private static ManualLogSource? logger;
 
         private void Awake()
         {
             Logger.LogInfo("Initializing ScaledLoot.");
-            logger = Logger;
             On.DepositItemsDesk.SellItemsOnServer += SellItemsOnServer;
 
             //configQuota = Config.Bind("General", "QuotaScale",2500,"The quota scale value.");
@@ -31,28 +26,20 @@ namespace ScaledLoot
 
         private static void SellItemsOnServer(On.DepositItemsDesk.orig_SellItemsOnServer orig, DepositItemsDesk self)
         {
-            if (logger != null)
+            TimeOfDay instance = TimeOfDay.Instance;
+            if (self.IsServer && instance != null)
             {
-                logger.LogInfo("Getting TimeOfDay");
-                TimeOfDay instance = TimeOfDay.Instance;
-                if (self.IsServer && instance != null)
+                int profQuota = TimeOfDay.Instance.profitQuota;
+                if (profQuota > QuotaScale) // make sure we don't divide because of <1 multiplicatives
                 {
-                    logger.LogInfo("Server check, checking profit quota");
-                    int profQuota = TimeOfDay.Instance.profitQuota;
-                    if (profQuota > QuotaScale) // make sure we don't divide because of <1 multiplicatives
+                    // correct scrap value multiplier
+                    int delta = profQuota / QuotaScale;
+                    foreach (GrabbableObject item in self.itemsOnCounter)
                     {
-                        logger.LogInfo("Using delta " + profQuota);
-                        // correct scrap value multiplier
-                        int delta = profQuota / QuotaScale;
-                        foreach (GrabbableObject item in self.itemsOnCounter)
-                        {
-                            item.SetScrapValue(item.scrapValue * delta);
-                        }
-                        logger.LogInfo("Scrap values updated");
+                        item.SetScrapValue(item.scrapValue * delta);
                     }
                 }
             }
-
 
             // call original method
             orig(self);
